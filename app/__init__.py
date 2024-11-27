076ebd2f636fe066c7024e18d175d4f9aad750a8
tresoraury <tresoraury@gmail.com>
2024-11-27 11:35:31 +0200

    migration

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_login import LoginManager
 from .models import db
 
 def create_app():
@@ -9,6 +10,14 @@ def create_app():
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
+
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        from .models import User
+        return User.query.get(int(user_id))
 
     with app.app_context():
         db.create_all()
