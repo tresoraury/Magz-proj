5e6a6c8d2de70bbf6d84b34905cfe8f7d6f951a2
tresoraury <tresoraury@gmail.com>
2024-12-30 18:25:51 +0200

    commit

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from .models import Item, User
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from .models import db
+from flask_restful import Api, Resource, reqparse
 import os
 
 
@@ -34,5 +35,28 @@ def create_app():
 
     from .routes import main  
     app.register_blueprint(main)  
+
+    api = Api(app)
+
+    from flask_restful import reqparse
+    parser = reqparse.RequestParser()
+    parser.add_argument('name', required=True)
+    parser.add_argument('description', required=True)
+
+    class ItemResource(Resource):
+        def get(self, item_id):
+            item = Item.query.get(item_id)
+            if item:
+                return {'id': item.id, 'name': item.name, 'description': item.description}
+            return {'message': 'Item Not Found'}, 404
+        
+        def post(self, item_id):
+            args = parser.parse_args()
+            new_item = Item(name=args['name'], description=args['description'])
+            db.session.add(new_item)
+            db.session.commit
+            return {'message': 'Item created', 'id': new_item.id}, 201
+        
+    api.add_resource(ItemResource, '/api/items', '/api/items/<int:item_id>')
 
     return app  
